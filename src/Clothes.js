38e8fe54ea414ba0e958963da1eb97a202aab2d2
Olha White <lechka1991@gmail.com>
2023-11-01 13:50:05 -0400

    latest updates

---
+++ src/Clothes.js
@@ -0,0 +1,17 @@
+function Clothes({showClothes}) {
+   return (
+    <div className="products">
+{showClothes.map((element => {
+    const {id, name, searchTerm, price, image} = element;
+    return <div className="product-card" key = {id}>
+        <img src = {image} width = "400px" height="500px" alt="clothes"></img>
+        <div className="product-info">
+        <h3>{name}</h3>
+        <h4>$ {price}</h4>
+        </div>
+    </div>
+}))}
+    </div>
+   ) 
+}
+export default Clothes;
